4355dcbfff886a69028a5f23beae1fb976adf45d
Huyvo307 <118785616+Huyvo307@users.noreply.github.com>
2023-07-01 22:16:56 +0700

    setup gihub action

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: ['/node_modules/(?!@react-navigation/elements)'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+    '@react-native/js-polyfills/error-guard.js':
+      '<rootDir>/node_modules/react-native/jest/transform.js',
+  },
 };
